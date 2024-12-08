a953ec78ffaf61100b303ba734d7b28747aa5681
humanediedofanxiety <saysayeem.pr@gmail.com>
2024-12-08 11:39:06 +0600

    fixed autocmd

--- init.lua
+++ init.lua
@@ -939,6 +939,7 @@ require('lazy').setup({
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
+  require 'custom.custom_autocmd',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
--- lua/custom/plugins/custom_opt.lua
+++
@@ -1,9 +0,0 @@
-return {
-  vim.api.nvim_create_autocmd('TermOpen', {
-    pattern = '*',
-    callback = function()
-      vim.opt_local.number = false
-      vim.opt_local.relativenumber = false
-    end,
-  }),
-}
