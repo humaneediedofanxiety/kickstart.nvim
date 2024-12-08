1e3d458cfb5193c7e6ed00345711b477eae69196
humanediedofanxiety <saysayeem.pr@gmail.com>
2024-12-08 14:52:37 +0600

    added keybindings for buffer and window management

--- lua/custom/plugins/mapping.lua
+++ lua/custom/plugins/mapping.lua
@@ -5,4 +5,14 @@
 
 return {
   vim.keymap.set('n', '<leader>th', '<cmd>Telescope colorscheme<CR>'),
+  vim.keymap.set('n', '<leader>ww', '<cmd>w<CR>'), -- save file
+
+  -- buffer and window management
+  vim.keymap.set('n', '<leader>tt', '<cmd>terminal<CR>'), -- open terminal
+  vim.keymap.set('n', '<leader>tb', '<cmd>new<CR>'), -- new buffer
+  vim.keymap.set('n', '<leader>td', '<cmd>bd!<CR>'), -- close buffer (force)
+  vim.keymap.set('n', '<leader>ts', '<cmd>split<CR>'), -- splits the window horiz
+  vim.keymap.set('n', '<leader>tv', '<cmd>vsplit<CR>'), -- splits the window vert
+  vim.keymap.set('n', '<leader>tq', '<cmd>q<CR>'), -- closes the current window
+  vim.keymap.set('n', '<leader>tz', '<cmd>new | terminal<CR>'), -- create new buffer and open terminal
 }
